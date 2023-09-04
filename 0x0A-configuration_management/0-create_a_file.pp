8a73067a5816043de4952ad58e60788cb9e0640b
jeshi2 <123796940+jeshi2@users.noreply.github.com>
2023-09-04 21:24:21 +0300

    Add files via upload

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file
+file { '/tmp/school':
+  ensure  =>  file,
+  path    =>  '/tmp/school',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'i love Puppet',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow.
+exec { 'killmenow':
+  command =>  'pkill -f killmenow',
+  path    =>   '/usr/bin:/bin',
+  onlyif  =>    'pgrep -f killmenow',
+}
