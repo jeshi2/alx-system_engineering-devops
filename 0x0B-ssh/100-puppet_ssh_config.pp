bc8a247d494725b453469aef62a3fdb97ff16c45
jeshi2 <123796940+jeshi2@users.noreply.github.com>
2023-09-04 21:27:38 +0300

    Add files via upload

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# using Puppet to make changes to our configuration file. 
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
+}
+
+service { 'ssh':
+  ensure => 'running',
+  enable => true,
+}
