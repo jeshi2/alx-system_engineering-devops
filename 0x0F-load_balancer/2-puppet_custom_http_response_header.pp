f843fe05664e4a490b701da39abaf5f79773fcaf
jeshi2 <jamesvean19@gmail.com>
2023-09-06 10:22:56 +0300

    python_networking

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,9 @@
 # 2-puppet_custom_http_response_header.pp
 
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
-}
-
-# Create a custom HTTP header file for Nginx
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => file,
-  content => "add_header X-Served-By $::hostname;",
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [
-    File['/etc/nginx/conf.d/custom_headers.conf'],
-  ],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
