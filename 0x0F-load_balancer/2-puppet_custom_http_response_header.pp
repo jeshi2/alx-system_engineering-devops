ff09bf3422f9941fc65ef6a0961d802091b52a9c
jeshi2 <jamesvean19@gmail.com>
2023-09-06 10:19:02 +0300

    python_networking

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,59 @@
-# Define a class to configure the custom HTTP header
-class custom_http_response_header {
+# 2-puppet_custom_http_response_header.pp
 
-  # Install Nginx
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Define a custom Nginx configuration file
-  file { '/etc/nginx/sites-available/custom-header':
-    ensure  => present,
-    content => "server {
-        listen 80;
-        server_name _;
-        add_header X-Served-By $hostname;
-        location / {
-            proxy_pass http://localhost:8080;
-        }
-    }",
-    require => Package['nginx'],
-  }
-
-  # Create a symbolic link to enable the custom site
-  file { '/etc/nginx/sites-enabled/custom-header':
-    ensure => link,
-    target => '/etc/nginx/sites-available/custom-header',
-    require => File['/etc/nginx/sites-available/custom-header'],
-  }
-
-  # Remove the default Nginx site
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => absent,
-    notify => Service['nginx'],
-  }
-
-  # Test Nginx configuration and reload Nginx
-  exec { 'nginx-config-test':
-    command => 'nginx -t',
-    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
-    notify  => Service['nginx'],
-  }
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-# Apply the custom HTTP header class
-include custom_http_response_header
+# Create a custom HTTP header file
+file { '/etc/nginx/custom_headers.conf':
+  ensure  => file,
+  content => "add_header X-Served-By $hostname;",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
 
+# Include the custom_headers.conf file in the Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('your_module/custom_headers.conf.erb'),
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+
+# Enable the custom header in Nginx configuration
+file { '/etc/nginx/sites-available/custom_headers.conf':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Remove the default Nginx default site configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure => absent,
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [
+    File['/etc/nginx/custom_headers.conf'],
+    File['/etc/nginx/sites-available/custom_headers.conf'],
+    File['/etc/nginx/sites-enabled/default'],
+  ],
+}
+
+# Restart Nginx when the custom header or configuration changes
+file { '/etc/nginx/custom_headers.conf':
+  notify => Service['nginx'],
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/custom_headers.conf':
+  notify => Service['nginx'],
+  require => Package['nginx'],
+}
