bc5147a67ca38379f7b36e1ac9d9c49bc15560cc
jeshi2 <jamesvean19@gmail.com>
2023-10-11 22:21:22 +0300

    web3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,12 @@
 # Ensure the missing file is present
+
 file { '/path/to/missing_file':
   ensure => file,
   source => 'puppet:///modules/mymodule/missing_file',
-  # You may need to adjust the source path and other attributes
 }
 
 # Optionally, restart Apache
+
 service { 'apache2':
   ensure  => 'running',
   enable  => true,
