243504d44d10e7c417a7c9500117139b5c10e3d8
MaureenMercy <jamesvean19@gmail.com>
2023-10-14 22:33:21 +0300

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles 
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Query the Reddit API to print the titles of the 
+    first 10 hot posts for a subreddit.
+
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyBot/1.0 (by YourUsername)'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+
+            for i, post in enumerate(posts[:10]):
+                print(f"{i+1}. {post['data']['title']}")
+        else:
+            print("None")
+    except Exception as e:
+        print("None")
+
+if __name__ == '__main':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        top_ten(subreddit)
