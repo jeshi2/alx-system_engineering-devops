af35d5bde7954ff652b9d3cef3d84e38a2bf5a41
jeshi2 <jamesvean19@gmail.com>
2023-10-22 07:56:46 +0300

    webde4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,26 +1,14 @@
-# 0-the_sky_is_the_limit_not.pp
+# Nginx is doing under pressure
 
-# Ensure Nginx is installed
-class { 'nginx':
-  ensure => 'installed',
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
 }
 
-# Define a virtual host for your web application
-nginx::resource::vhost { 'my_website':
-  ensure     => present,
-  listen_ip  => '0.0.0.0',
-  listen_port => '80',
-  server_name => 'localhost',
-  location   => {
-    '/' => {
-      proxy => 'http://127.0.0.1:8080', # Change the proxy as needed
-    },
-  },
-}
+#restart Nginx
 
-# Restart Nginx to apply the changes
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Nginx::Resource::Vhost['my_website'],
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
